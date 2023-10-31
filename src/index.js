9edf23d3433216013ab4683aa516ab52d1631afa
Alexander <skidan2908@gmail.com>
2023-10-31 11:43:03 +0300

    create react app & install redux react-redux & test redux

--- src/index.js
+++ src/index.js
@@ -3,25 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
 import {Provider} from "react-redux";
-
-
-const defaultState = {
-    cash: 0,
-}
-const reducer = (state = defaultState, action) => {
-    switch (action.type) {
-        case 'ADD_CASH' :
-            return {...state, cash: state.cash + action.payload};
-        case 'GET_CASH' :
-            return {...state, cash: state.cash - action.payload};
-        default:
-            return state;
-    }
-}
-
-const store = createStore(reducer);
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
